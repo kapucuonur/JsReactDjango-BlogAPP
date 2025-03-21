78bb52edfdaafcfe4137de1acaf01722eda0e9af
Onur Kapucu <kapucuonur@hotmail.com>
2025-03-21 11:40:29 +0200

    updates

--- .history/backend/backend/urls_20250321113936.py
+++ .history/backend/backend/urls_20250321113936.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
