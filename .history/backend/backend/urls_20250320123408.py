3a7be20e54256ada2ad93b1c431882f657ca25be
Onur Kapucu <kapucuonur@hotmail.com>
2025-03-22 19:24:18 +0200

    Save current changes

--- .history/backend/backend/urls_20250320123408.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
-    path('api/auth/', obtain_auth_token, name='api-token-auth'),
-]
